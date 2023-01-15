37b0d3fdb4725b1ecf43b81c74efb06444d8826f
Trudy Allred <all22030@byui.edu>
2023-01-14 20:18:24 -0700

    adding Entry Class & Prompt Class

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,54 +1,84 @@
 using System;
 
-
-// string filename = "prompts.txt";
-// string[] lines = System.IO.File.ReadAllLines(filename);
-
-// foreach (string line in lines)
-// {
-//     string[] parts = line.Split("|");
-
-//     string prompt1 = parts[0];
-//     string prompt2 = parts[1];
-//     Console.WriteLine($"{prompt1}");
-// }
-
-class promptS
+class Program
 {
     static void Main(string[] args)
     {
-        Random randomGenerator = new Random();
+        int selection = 0;
+        do
+        {
+            // Task options to choose from.
+            Console.WriteLine("Welcome to Your Journal!");
+            Console.WriteLine("Please select from the following options:");
+            Console.WriteLine("1. Write ");
+            Console.WriteLine("2. Display ");
+            Console.WriteLine("3. Load ");
+            Console.WriteLine("4. Save ");
+            Console.WriteLine("5. Quit ");
+            Console.Write("What would you like to do? ");
 
-        string filename = "prompts.txt";
-        string[] lines = System.IO.File.ReadAllLines(filename);
+            //Read input and change to interger.
+            string choice = Console.ReadLine();
+            selection = int.Parse(choice);
 
-        int count = 0;
+            // check it is working
+            // Console.Write(choice);
 
+            if (selection == 1)
+            {
+                Entry newEntry = new Entry();
+                // newEntry._choice = 1;
+                newEntry.Display();
+            }
+        }
+        while (selection != 5);
+        Console.WriteLine("Thanks for using this program");
+    }
+}
+
+public class Entry
+{
+    public string _writing;
+
+    public void Display()
+    {
+        {
+            Prompt newPrompt = new Prompt();
+            newPrompt.Display();
+
+            // Console.Write("> ");
+            // _writing = Console.ReadLine();
+            // Console.WriteLine(_writing);
+        }
+    }
+}
+
+public class Prompt
+{
+    Random randomGenerator = new Random();
+
+    string filename = "prompts.txt";
+    string[] lines = System.IO.File.ReadAllLines(filename);
+
+    int count = 0;
+
+    public void Display()
+    {
+            // Prompt newPrompt = new Prompt();
+            // newPrompt._prompt = 0;
+            // newPrompt.Display();
         foreach (string line in lines)
         {
             count = count + 1;
             string[] parts = line.Split("|");
-        }
+        
         int number = randomGenerator.Next(count);
 
         string prompt = parts[number];
 
+
         Console.WriteLine($"{prompt}");
-
-        // foreach (string line in lines)
-        // {
-        //     string[] line = line.Split("|");
-
-        //     string first = parts[0];
-        //     Console.Write($"{parts[0]}");
-            // string second = parts[1];
-            // Console.Write($"{parts[1]}");
-
-            // string prompt = parts[3];
         }
-
-        // Console.WriteLine($"{prompt}");
-
     }
+        
 }
-
